ef82ab002c05a8b9b7a5957f574fbbe1561f4532
Areeba Riaz <160718011+Areeba-majid@users.noreply.github.com>
2024-03-18 01:26:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstnumber" },
+    { message: "Enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "Select one of the operators to perform operator",
+        type: "list",
+        name: "operators",
+        choices: ["addition", "Subtraction", "multiplication", "division"],
+    },
+]);
+console.log(answer);
